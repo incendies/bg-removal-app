b63a10274ca575398728da53d1c49e4c7ec2e860
Yunus Emre Özdiyar <yunusemreozdiyar@gmail.com>
2024-09-19 22:01:39 +0200

    remove processed images from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { ImageUploader } from "@/components/image-uploader";
-import { Dashboard } from "@/components/dashboard";
+// import { Dashboard } from "@/components/dashboard";
 
 export default function Home() {
   return (
     <div>
       <ImageUploader />
-      <Dashboard />
+      {/* <Dashboard /> */}
     </div>
   );
 }
